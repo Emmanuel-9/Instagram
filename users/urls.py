a9c6595ffddf2ad8d5628912f02dc9ba53eeed17
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-12 11:17:41 +0300

    Add the search and profile url functions.

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns=[
     url('^$',views.home, name='Home'),
-    url(r'^images/',views.images, name='Images')
+    url(r'^images/',views.images, name='Images'),
+    url(r'^search/', views.search_results, name='search_results'),  
+    url(r'^profile/',views.profile, name='Profile')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
