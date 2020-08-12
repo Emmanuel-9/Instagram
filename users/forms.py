e9e403c76751c9d0a280b350262c90b95f20d84a
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-12 11:15:22 +0300

    Add the ImagesForm.

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,17 @@
 from django import forms
+from .models import Images
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit,Layout,Field
 
-class LoginForm(forms.Form):
-    pass
+class ImagesForm(forms.Form):
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.add_input(Submit('Post','Post',css_class='btn-primary'))
+
+    class Meta:
+        model = Images
+        fields = {
+            'image'
+            'caption'
+        }
 
